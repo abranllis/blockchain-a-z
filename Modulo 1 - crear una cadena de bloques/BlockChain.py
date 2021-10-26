736fe5373dcb53215ad6d78bbed735dd56365052
abranllis <47560487+abranllis@users.noreply.github.com>
2021-10-26 10:58:26 +0200

    Update BlockChain.py

--- Modulo 1 - crear una cadena de bloques/BlockChain.py
+++ Modulo 1 - crear una cadena de bloques/BlockChain.py
@@ -6,3 +6,14 @@ Created on Mon Oct 25 16:54:58 2021
 """
 
 # Módulo 1 - Crear una cadena de Bloques
+
+#Para instalar:
+# Flask==0.12.2: pip install Flask=0.12.2
+#cliente HTTP Postman: https://www.getpostman.com/
+
+#importar las librerias
+
+#Parte 1 - Crear la cadena de bloques
+
+#Parte 2 - Minado de un bloque de la cadena
+
